2e0776995ca40650e9e5d88575ca4aceef568d5c
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-28 20:50:27 +0300

    Initial Commit

---
+++ VPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VPN
+//
+//  Created by Алексей Гуляев on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
